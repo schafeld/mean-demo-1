21726fdf4fec99cc683a58eb7c1dfef1d31f8811
Oliver Schafeld <oliver@schafeld.com>
2016-08-22 21:10:59 +0200

    add Imgur scraper.

--- server/api/imgScraper/imgScraper.controller.js
+++ server/api/imgScraper/imgScraper.controller.js
@@ -3,6 +3,7 @@
 var scrapers = {};
 
 scrapers['pinterest'] = require('./scrapers/pinterest.js');
+scrapers['imgur'] = require('./scrapers/imgur.js');
 
 exports.scrape = function(req, res) {
   var url = req.body.url;
@@ -10,6 +11,8 @@ exports.scrape = function(req, res) {
 
   if (url.indexOf('pinterest') > -1) {
     scraperToUse = 'pinterest';
+  } else if (url.indexOf('imgur') > -1) {
+    scraperToUse = 'imgur';
   } else {
     console.log('Cannot locate scraper.');
   }
---
+++ server/api/imgScraper/scrapers/imgur.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.list = function(url, cb) {
+  request(url, function(error, resp, body) {
+    if (error) {
+      cb({
+        error: error
+      });
+    }
+    if (!error) {
+      var $ = cheerio.load(body);
+      var $url = url;
+      var $img = $('img.js-post-image-thumb').attr('src');
+      var $desc = $('img.js-post-image-thumb').attr('alt'); // description from Imgur
+
+      console.log($img + ' Imgur image url');
+
+      var image = {
+        img: 'http:' + $img,
+        url: $url,
+        desc: $desc
+      };
+
+      // respond with final JSON object
+      console.log('Scraped: ' + image);
+      cb(image);
+    }
+  });
+};
